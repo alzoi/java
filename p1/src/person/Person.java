231d1a7bdddf82de1b7c8385468af9c2174e4c15
alzoi <alzoi@ya.ru>
2021-05-10 18:49:11 +0500

    Create Person.java

---
+++ p1/src/person/Person.java
@@ -0,0 +1,12 @@
+package person;
+
+public class Person {
+  private String Name;
+  
+  public String getName() {
+	  return Name;
+  }
+  public void setName(String p_name) {
+	  Name = p_name;
+  }
+}
