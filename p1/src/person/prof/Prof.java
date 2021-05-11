84f1c0a0981fd6344384c97cf6a35b255569feaf
alzoi <alzoi@ya.ru>
2021-05-11 11:38:56 +0500

    Update Prof.java

--- p1/src/person/prof/Prof.java
+++ p1/src/person/prof/Prof.java
@@ -1,12 +1,12 @@
 package person.prof;
 
 public class Prof {
-  private String Name;
+	private String Name;
   
-  public String getName() {
-	  return Name;
-  }
-  public void setName(String p_name) {
-	  Name = p_name;
-  }
+	public String getName() {
+		return Name;
+	}
+	public void setName(String p_name) {
+		Name = p_name;
+	}
 }
