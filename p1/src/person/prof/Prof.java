b7aca1b3591db90b81b318e4a3019d2bc35dd56d
alzoi <alzoi@ya.ru>
2021-05-11 11:36:54 +0500

    Create Prof.java

---
+++ p1/src/person/prof/Prof.java
@@ -0,0 +1,12 @@
+package person.prof;
+
+public class Prof {
+  private String Name;
+  
+  public String getName() {
+	  return Name;
+  }
+  public void setName(String p_name) {
+	  Name = p_name;
+  }
+}
